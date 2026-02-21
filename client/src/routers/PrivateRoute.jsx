66d9cbf50430533b9b523b965820da51e43fe6a9
Md. Toyob Uddin Hridoy <hridoykhan148385@gmail.com>
2026-02-21 17:13:48 +0600

    private route updated

--- client/src/routers/PrivateRoute.jsx
+++ client/src/routers/PrivateRoute.jsx
@@ -1,16 +1,13 @@
 import { Navigate, useLocation } from "react-router";
 import useAuthValue from "../hooks/useAuthValue";
+import ErrorLoadingState from "../components/ErrorLoadingState";
 
 const PrivateRoute = ({ children }) => {
   const location = useLocation();
   const { loading, user } = useAuthValue();
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center min-h-50">
-        <span className="loading loading-dots loading-lg text-primary"></span>
-      </div>
-    );
-  }
+   if (loading) {
+     return <ErrorLoadingState isPending={true} />;
+   }
   if (user) return children;
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
