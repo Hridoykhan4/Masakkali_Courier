e60b173482e9b29e24c5f7b8b95f273f816821ac
Md. Toyob Uddin Hridoy <hridoykhan148385@gmail.com>
2026-02-28 04:56:00 +0600

    removed unncesseary assets

--- client/src/firebase/firebase.init.js
+++ client/src/firebase/firebase.init.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// Masakkali config in firebase
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
   authDomain: import.meta.env.VITE_authDomain,
@@ -13,6 +10,6 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_appId,
 };
 
-// Initialize Firebase
+// Initializing Firebase
 const app = initializeApp(firebaseConfig);
 export default app;
